b9895973eefd5610e1b7c81a2008f34f7ab45c7e
takagotch <dyaccb@gmail.com>
2018-06-30 22:34:45 +0900

    if ( % 5 == 0) {

--- fb-linked.php
+++ fb-linked.php
@@ -52,18 +52,28 @@ echo '<hr />'."\n";
 
 
 if($user){
-  echo ''."";
-  echo ''.$user .''."";
+  echo '<h3></h3>'."\n";
+  echo '<img src="https://graph.facebook.com/' .$user .'/picture'. "\n";
 
-  echo ''."";
-  echo ''."";
-  echo ''."";
-  echo ''."";
+  echo '<h3>Login info person</h3>'."\n";
+  echo '<pre>'."\n";
+  echo print_r($user_profile);
+  echo '</pre>'."\n";
 
-  echo '';
-  echo '';
+  echo '<h3>Login friends list</h3>';
+  $user_friends_data = $user_friends['data'];
+  echo '<h4>frineds Number:'. count($user_frineds_data) . ' person</h4>'. "\n";
+  $i=0;
+  foreach ($user_frineds_data as $fkey=>fvalue){
+    $i++;
+    echo '<a href="http://www.facebook.com/profile.php?id='.$fvalue[id].'"><img src="https://graph.facebook.com/' .$fvalue[id] . '/picture" border="0" title=""' .$fvalue[name].'"/></a>';
+    if($i % 5 == 0){
+      echo '<br><br>';
+    }
+  }
+
 } else {
-  echo '';
+  echo '<strong><em>NOT LOGIN</em></strong>'."\n";
 }
 
 echo<<<_FOOTER_
